5b45b17524746cdb0e6b12af268154aa9a4ed96d
Extension1990 <extensiontlhareseng@gmail.com>
2026-03-13 13:06:12 +0200

    Auth pages design done

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Button } from '../../shared/components/button/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [Button],
+  imports: [Button, RouterLink],
   templateUrl: './login.html',
   host: {
     class: 'min-h-screen flex items-center justify-center bg-slate-200 p-4'
--- frontend/src/app/pages/register/register.ts
+++ frontend/src/app/pages/register/register.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Button } from '../../shared/components/button/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [Button, RouterLink],
   templateUrl: './register.html',
+  host: {
+    class: 'min-h-screen flex items-center justify-center bg-slate-200 p-4'
+  },
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+  
+}
