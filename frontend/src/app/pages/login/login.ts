5a135457dcef20b7dce660ab0b676c40433ca645
Extension1990 <extensiontlhareseng@gmail.com>
2026-03-13 13:46:04 +0200

    Added error messages on the login form

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Button } from '../../shared/components/button/button';
 import { RouterLink } from '@angular/router';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [Button, RouterLink],
+  imports: [Button, RouterLink, FormField, FormsModule],
   templateUrl: './login.html',
   host: {
     class: 'min-h-screen flex items-center justify-center bg-slate-200 p-4'
@@ -12,5 +14,23 @@ import { RouterLink } from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
-  
+  loginModel = signal({
+    username: '',
+    password: ''
+  });
+
+  loginForm = form(this.loginModel, (rootPath) => {
+    required(rootPath.username, {message: 'Username is required.'});
+    required(rootPath.password, {message: 'Password is required.'});
+    minLength(rootPath.password, 6, {message: 'Password must be atleast 6 characters long.'})
+  });
+
+  login(event: Event) {
+    event.preventDefault();
+    if(this.loginForm().valid()) {
+       console.log('Login Data', this.loginForm().value());
+    } else {
+      console.log('Login form invalid!');
+    }
+  }
 }
